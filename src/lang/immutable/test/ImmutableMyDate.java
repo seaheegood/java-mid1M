bca3ff065a8c74c5220b9fff9bb933e3021bc12c
Sehee Hong <seaheehong@gmail.com>
2024-05-25 01:53:05 -0400

    불변객체로 만들고 필드값을 바꾸는 메서드를 통해서 해결

---
+++ src/lang/immutable/test/ImmutableMyDate.java
@@ -0,0 +1,31 @@
+package lang.immutable.test;
+
+public class ImmutableMyDate {
+
+    private final int year;
+    private final int month;
+    private final int day;
+
+    public ImmutableMyDate(int year, int month, int day) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+
+    public ImmutableMyDate changeYear(int year) {
+        return new ImmutableMyDate(year, month, day);
+    }
+
+    public ImmutableMyDate changeMonth(int month) {
+        return new ImmutableMyDate(year, month, day);
+    }
+
+    public ImmutableMyDate changeDay(int day) {
+        return new ImmutableMyDate(year, month, day);
+    }
+
+    @Override
+    public String toString() {
+        return year + "-" + month + "-" + day;
+    }
+}
