bbc5a7cbfb45cc689d0256f1d58dee32bf679123
Sehee Hong <seaheehong@gmail.com>
2024-05-26 22:52:34 -0400

    String Class - 기본

---
+++ src/lang/string/CharArrayMain.java
@@ -0,0 +1,12 @@
+package lang.string;
+
+public class CharArrayMain {
+
+    public static void main(String[] args) {
+        char[] charArr = new char[]{'a', 'b', 'c', 'd', 'e', 'f'};
+        System.out.println(charArr);
+
+        String str = "abcdef";
+        System.out.println("str: " + str);
+    }
+}
