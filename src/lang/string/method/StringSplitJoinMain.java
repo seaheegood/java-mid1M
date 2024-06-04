963412066eeaca6d064ea0e1aecf0db6b8f2a64f
Sehee Hong <seaheehong@gmail.com>
2024-06-03 23:46:11 -0400

    String Class - 주요 메서드2: split, join

---
+++ src/lang/string/method/StringSplitJoinMain.java
@@ -0,0 +1,28 @@
+package lang.string.method;
+
+public class StringSplitJoinMain {
+
+    public static void main(String[] args) {
+        String str = "Apple,Banana,Orange";
+
+        // split()
+        String[] splitStr = str.split(",");
+        for (String s : splitStr) {
+            System.out.println(s);
+        }
+
+        String joinStr = "";
+        for (int i=0; i<splitStr.length; i++) {
+            String string = splitStr[i];
+            joinStr += string;
+            if (i != splitStr.length - 1) {
+                joinStr += "-";
+            }
+        }
+        System.out.println("joinStr: " + joinStr);
+
+        // join()
+        String joinedStr = String.join("-", "A", "B", "C");
+        System.out.println("joinedStr: " + joinedStr);
+    }
+}
